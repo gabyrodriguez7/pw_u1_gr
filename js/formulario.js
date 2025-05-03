335a56ff1681fbbc5432c438740b48574899b3da
gabyrodriguez7 <167278578+gabyrodriguez7@users.noreply.github.com>
2025-05-03 18:06:43 -0500

    Actualizacion de validaciones

--- js/formulario.js
+++ js/formulario.js
@@ -4,11 +4,38 @@ function guardar() {
 
 function validar() {
     let nombre = document.getElementById("id_nombre").value;
+    let apellido = document.getElementById("id_apellido").value;
+    let fechaDeNacimiento = document.getElementById("id_fecha_nacimiento").value;
+    let email = document.getElementById("id_email").value;
+    let password = document.getElementById("id_password").value;
 
     if (nombre == "") {
         console.log("Error no ha ingresado el nombre");
     } else{
         console.log("Paso validacion del nombre");
-
+    }
+    
+    if (apellido == "") {
+        console.log("Error no ha ingresado el apellido");
+    } else{
+        console.log("Paso validacion del apellido");
+    }
+    
+    if (fechaDeNacimiento == "") {
+        console.log("Error no ha ingresado el fecha de nacimiento");
+    } else{
+        console.log("Paso validacion del fecha de nacimiento");
+    }
+    
+    if (email == "") {
+        console.log("Error no ha ingresado el email");
+    } else{
+        console.log("Paso validacion del email");
+    }
+    
+    if (password == "") {
+        console.log("Error no ha ingresado el password");
+    } else{
+        console.log("Paso validacion del password");
     }
 }
